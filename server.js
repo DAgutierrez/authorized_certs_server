c8f98d3eb485fc0866bbc8063dd250a91a384d3e
DAGUTIERREZ <diego.gutierrez684@gmail.com>
2019-11-27 17:21:03 -0300

    adding initial api

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('init');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(bodyParser.json({ strict: false, limit: '50mb'}));
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(3000, () => {
+    console.log('listen on 3000')
+})
